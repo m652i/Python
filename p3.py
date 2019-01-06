279109cdf4da555b17447c4438785fafff3dba6c
Mike <46409382+m652i@users.noreply.github.com>
2019-01-05 20:35:35 -0500

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,11 @@
+print('Program will calculate the average of the first three number,')
+print('and compare with the fourth number.')
+a = float(input('Enter the first number: '))
+b = float(input('Enter the second number: '))
+c = float(input('Enter the third number: '))
+d = float(input('Enter the fourth number: '))
+if (a + b + c)/3 == d:
+    print('Equal')
+else:
+    print('Not equal')
+print('Done.')
